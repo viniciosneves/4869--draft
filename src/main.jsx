e693fc64707c28970f9ca7dd1d909556c298e51d
Vinicios Neves <vinicios.neves@mac>
2025-08-01 12:11:47 +0100

    video 1.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { BrowserRouter, Route, Routes } from "react-router";
+
+
 import { Login } from './pages/Login/index.jsx'
 import { Register } from './pages/Register/index.jsx'
 import { Feed } from './pages/Feed/index.jsx'
@@ -8,9 +12,19 @@ import { BlogPost } from './pages/BlogPost/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/register" element={<Register />} />
+      
+      <Route path="/" element={<Feed />} />
+      <Route path="/blog-post" element={<BlogPost />} />
+      
+    </Routes>
+
     {/* <Register /> */}
     {/* <Feed /> */}
     {/* <BlogPost /> */}
+    </BrowserRouter>
   </StrictMode>,
 )
